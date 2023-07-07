e84e876362fcbaf7aabeef647576bd3a0b25d936
AmitxD <81667979+Amitminer@users.noreply.github.com>
2023-07-07 19:47:28 +0530

    Create Utils.php

---
+++ src/Biswajit/libs/davidglitch04/libEco/Utils/Utils.php
@@ -0,0 +1,14 @@
+<?php 
+
+namespace Biswajit\libs\davidglitch04\libEco\Utils;
+
+final class Utils {
+
+    public function __construct(){
+        //NOTHING
+    }
+
+	public const ECONOMYAPI = "EconomyAPI";
+	
+	public const BEDROCKECONOMYAPI = "BedrockEconomyAPI";
+}
